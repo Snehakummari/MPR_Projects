76e7b06d43839b50e63d3b1c475fac7bb03829ff
Snehakummari <165374965+Snehakummari@users.noreply.github.com>
2026-03-05 22:24:17 +0530

    Delete GST Assistant Chatbot directory

--- GST Assistant Chatbot/chatbot.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { handleMessage } = require("../controllers/chatbot.controller");
-
-const router = express.Router();
-
-router.post("/chat", handleMessage);
-
-module.exports = router;
